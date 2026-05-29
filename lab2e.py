a821025b0e861ebbfb9285036ca30ec51977db25
Namra Patel <nrpatel52@myseneca.ca>
2026-05-29 13:55:12 -0400

    Completed Lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Namra Patel
+# Author ID: nrpatel52
+# Date Created: 2026/05/22
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
---
+++ lab2g.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Author: Namra Patel
+# Author ID: nrpatel52
+# Date Created: 2026/05/22
+
+import sys
+
+if len(sys.argv) == 1:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
